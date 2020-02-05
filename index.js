a04907bb8fe590cba586174e18b4790f22b54367
Grant <50247537+mccorveyit@users.noreply.github.com>
2020-02-05 15:30:12 -0600

    It works

--- index.js
+++ index.js
@@ -9,48 +9,25 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 const index = async () => {
 
-    const employeesSheet = await ss.sheets.getSheet({ id: 7831794801239940 });
-    const employeeColumn = employeesSheet.columns.find(column => column.title === "Name");
-    const employees = employeesSheet.rows.map(row => row.cells.find(cell => cell.columnId === employeeColumn.id).value);
-
-
-    const projectsSheet = await ss.sheets.getSheet({ id: 4101959302047620 });
-    const projectColumn = projectsSheet.columns.find(column => column.title === "Project Name");
-    const projects = projectsSheet.rows.map(row => row.cells.find(cell => cell.columnId === projectColumn.id).value);
-
+    // const productionLogSheet = await ss.sheets.getSheet({ id: 8894448227641220});
+    // const typeColumn = productionLogSheet.columns.find(column => column.title === "Type");
+    // const types = productionLogSheet.rows.map(row => row.cells.find(cell => cell.columnId === typeColumn.id).value);
+    const types = ['MECH','PLMB','HNGR', 'CUST', 'SLV'];
 
     const sheets = (await Promise.all(
-        (await s3.listObjectsV2({ Bucket: process.env.BUCKET_SHEETS, Prefix: 'Timesheet' }).promise()).Contents
-            .filter(object => object.Key.includes('Timesheet'))
+        (await s3.listObjectsV2({ Bucket: process.env.BUCKET_SHEETS, Prefix: 'Production_Log' }).promise()).Contents
+            .filter(object => object.Key.includes('Production_Log'))
             .map(({ Key }) => s3.getObject({ Bucket: process.env.BUCKET_SHEETS, Key }).promise().then(row => JSON.parse(row.Body.toString())))
     ))
     for (const sheet of sheets) {
-        // const sheet = await ss.sheets.getSheet({id: 8605558929418116});
-        debugger;
-        const columnEmployees = sheet.columns.find(column => column.title === "Employee").options;
-        if (JSON.stringify(columnEmployees) !== JSON.stringify(employees)) {
+        const columnType = sheet.columns.find(column => column.title === "Type").options;
+        if (JSON.stringify(columnType) !== JSON.stringify(types)) {
             await ss.sheets.updateColumn({
                 sheetId: sheet.id,
-                columnId: sheet.columns.find(column => column.title === "Employee").id,
+                columnId: sheet.columns.find(column => column.title === "Type").id,
                 body: {
-                    // "index": 0,
-                    // "title": "First Column",
                     "type": "PICKLIST",
-                    "options": employees
-                }
-            })
-        }
-
-        const columnProjects = sheet.columns.find(column => column.title === "Project").options;
-        if (JSON.stringify(columnProjects) !== JSON.stringify(projects)) {
-            await ss.sheets.updateColumn({
-                sheetId: sheet.id,
-                columnId: sheet.columns.find(column => column.title === "Project").id,
-                body: {
-                    // "index": 0,
-                    // "title": "First Column",
-                    "type": "PICKLIST",
-                    "options": projects
+                    "options": types
                 }
             })
         }
